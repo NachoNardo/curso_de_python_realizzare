aca09eaf02e01b7de26653dc50ff8b62e606884b
Leonardo Holanda e Silva <44146880+NachoNardo@users.noreply.github.com>
2023-06-26 21:49:21 -0300

    Update operadores.py

--- 1_variaveis/operadores.py
+++ 1_variaveis/operadores.py
@@ -10,76 +10,76 @@
 x = 1 + 1
 print(x)
 
-# x = 2 - 2
-# print(x)
-#
-# x = 3 * 3
-# print(x)
-#
-# x = 4 / 4
-# print(x)
-#
-# x = 5 % 2
-# print(x)
-#
-# x = 5 // 2
-# print(x)
-#
-# x = 5 ** 2
-# print(x)
-#
-# # Atribuições automáticas
-# # += valor anterior mais
-# # -= valor anterior menos
-# # *= valor anterior vezes
-# # /= valor anterior dividido
-# # %= valor anterior resto de divisão
-#
-# x = 1
-# x += 1 #x = x + 1
-# print(x)
-#
-# x -= 1 #x = x - 1
-# print(x)
-#
-# x *= 4 #x = x * 4
-# print(x)
-#
-# x /= 2 #x = x / 2
-# print(x)
-#
-# x += 3
-# x %= 2 #x = x % 2
-# print(x)
-#
-# # Operadores lógicos
-# # >  Maior que
-# # <  Menor que
-# # == Igual a
-# # != Diferente de
-# # >= Maior ou igual a
-# # <= Menor ou igual a
-#
-# v = 1 > 2  # False
-# print(x)
-# v = 1 < 2  # True
-# print(x)
-# v = 2 == 2 # True
-# print(x)
-# v = 2 != 3 # True
-# print(x)
-# v = 1 >= 5 # False
-# print(x)
-# v = 1 <= 5 # True
-# print(x)
-#
-# # and -> Falso se um é falso
-# # or  -> Verdadeiro se um é verdadeiro
-# # not -> Lógica inversora
-#
-# v = True and False
-# print(x)
-# v = True or False
-# print(x)
-# v = not True
-# print(x)
+x = 2 - 2
+print(x)
+
+x = 3 * 3
+print(x)
+
+x = 4 / 4
+print(x)
+
+x = 5 % 2
+print(x)
+
+x = 5 // 2
+print(x)
+
+x = 5 ** 2
+print(x)
+
+# Atribuições automáticas
+# += valor anterior mais
+# -= valor anterior menos
+# *= valor anterior vezes
+# /= valor anterior dividido
+# %= valor anterior resto de divisão
+
+x = 1
+x += 1 #x = x + 1
+print(x)
+
+x -= 1 #x = x - 1
+print(x)
+
+x *= 4 #x = x * 4
+print(x)
+
+x /= 2 #x = x / 2
+print(x)
+
+x += 3
+x %= 2 #x = x % 2
+print(x)
+
+# Operadores lógicos
+# >  Maior que
+# <  Menor que
+# == Igual a
+# != Diferente de
+# >= Maior ou igual a
+# <= Menor ou igual a
+
+v = 1 > 2  # False
+print(x)
+v = 1 < 2  # True
+print(x)
+v = 2 == 2 # True
+print(x)
+v = 2 != 3 # True
+print(x)
+v = 1 >= 5 # False
+print(x)
+v = 1 <= 5 # True
+print(x)
+
+# and -> Falso se um é falso
+# or  -> Verdadeiro se um é verdadeiro
+# not -> Lógica inversora
+
+v = True and False
+print(x)
+v = True or False
+print(x)
+v = not True
+print(x)

b0db306a208d4f03c6dd70424f199165d61af618
Leonardo Holanda e Silva <44146880+NachoNardo@users.noreply.github.com>
2023-08-20 21:46:12 -0300

    Add files via upload

---
+++ Exercicios/exercicios_if_estruturas_repeticao.py
@@ -0,0 +1,18 @@
+from random import randint
+
+# A função randint() recebe dois números inteiros como parâmetros
+# e retorna um número inteiro aleatório entre eles.
+# Usando a função como base, crie um jogo onde é escolhido um número aleatório de 1 a 10
+# dê a chance para a pessoa chutar 5 palpites, se ela acertar ganhou, senão diga que perdeu e revele
+
+var = randint(1,10)
+
+# Utilize a lista abaixo de notas de uma sala e diga quantos alunos foram aprovados
+# Primeiramente caso a média para passar seja 6.0, e depois 7.0
+
+lista = [6.0, 5.4, 7.1, 8.0, 9.0, 0.0, 1.2, 3.5, 8.7, 10.0]
+
+# Crie um menu de ações que se repitam indefinidamente
+# onde ao apertar 1 a pessoa receba a mensagem olá mundo
+# ao apertar 2 ela receberá a mensagem tchau mundo
+# e ao apertar 3 você sairá da repetição
---
+++ Exercicios/exercicios_orientacoes_objetos.py
@@ -0,0 +1,9 @@
+# Crie um objeto do tipo aluno que tenha atributos nome e nota
+# A nota deve iniciar em 0.0 e o nome deve ser inserido pelo usuário
+# Crie um método estudar e sempre que ele for executado a nota aumenta em 0.5 até o total de 10
+# Crie um método chamado fazer prova
+# caso sua nota seja menor que 6.0 ele deve receber a mensagem reprovado
+# senão ele deve receber a mensagem aprovado
+
+
+

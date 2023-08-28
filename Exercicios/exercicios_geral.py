5e03e7d025ef8dbfc2ecd26ebdb198837cf9e4e0
Leonardo Holanda e Silva <44146880+NachoNardo@users.noreply.github.com>
2023-08-28 18:45:06 -0300

    Add files via upload

---
+++ Exercicios/exercicios_geral.py
@@ -0,0 +1,8 @@
+# Crie um sistema que gere uma escola
+# Esta escola possuirá 10 alunos no total,
+# Cada aluno terá uma nota de 0 a 10 em valor flutuante
+# No sistema você poderá inserir um aluno ou remover
+# O sistema irá rodar em um menu que deve se repetir após cada operação
+# Deve haver as opções de inserir aluno, remover aluno e sair do sistema
+# Ao decidir sair do sistema você deve simplesmente sair do menu
+
